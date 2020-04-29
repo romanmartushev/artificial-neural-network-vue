0a84ace890caca89ebf0f9460de10a27e6caca63
Roman <romanm@paulbunyan.net>
2020-04-29 13:05:10 -0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+export default new Vue({
+  el: '#app',
+  data: {
+
+  },
+  mounted() {
+    console.log('hello world');
+  }
+});
---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('./public_html');
+
+mix.options({
+  processCssUrls: false
+});
+
+mix.js('resources/js/app.js', '/js')
+    .sass('resources/sass/app.scss', './public_html/css');
